dbbc3ceaa31c72fdf2133fa1342a5d381ad54d8d
Agnes Chueng <agneschueng@gmail.com>
2020-10-23 18:37:49 -0500

    routes for the files

---
+++ server/routes.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const sendDir = path.resolve(__dirname, "/..", "/public");
+
+function routes(server) {
+    server.get("/", function(req, res) {
+        res.sendFile(path.join(sendDir, "index.html"));
+    });
+
+    server.get("/notes", function(req, res) {
+        res.sendFile(path.join(sendDir, "notes.html"));
+    });
+}
+
+module.exports = routes;
