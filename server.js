3dea0453ac6efa03a775b85fb19671c25070e68d
Agnes Chueng <agneschueng@gmail.com>
2020-10-25 18:40:41 -0500

    cleaned up commented console.log()

--- server.js
+++ server.js
@@ -19,21 +19,17 @@ routes(app);
 app.post("/api/notes", function(req, res) {
     fs.readFile("./db/db.json", "utf8", function(error, data) {
         if(error) throw error;
-        // console.log(data);
         // Parse data to manipulate the array
         const noteList = JSON.parse(data);
         noteList.push(req.body);
-        // console.log(noteList);
         // Loop over updated array, and create a new key value pair with a property 'id', and a value of the index of each individual note object
         noteList.map(note => {
             note.id = noteList.indexOf(note);
         });
-        // console.log(noteList);
         const newNoteList = JSON.stringify(noteList);
         // Write the new data to db.json
         fs.writeFile("./db/db.json", newNoteList, function(error) {
             if(error) throw error;
-            // console.log("Success!");
             res.json(newNoteList);
         })
     })
@@ -41,9 +37,7 @@ app.post("/api/notes", function(req, res) {
 
 // Get requests will go here
 app.get("/api/notes", function(req, res){
-    // console.log("Yes!");
     fs.readFile("./db/db.json", "utf8", function(error, data){
-        // console.log(data);
         if (error) throw error;
         res.json(JSON.parse(data));
     })
