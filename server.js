81dc73b2042908d7dc9f31fd76298c5783b6e41b
Agnes Chueng <agneschueng@gmail.com>
2020-10-25 21:50:20 -0500

    changed for loop to forEach loop

--- server.js
+++ server.js
@@ -51,11 +51,11 @@ app.delete("/api/notes/:id", function(req, res){
     fs.readFile("./db/db.json", "utf8", function(error, data){
         if(error) throw error;
         const allNotes = JSON.parse(data);
-        for (let i=0; i<allNotes.length; i++) {
-            if (allNotes[i].id == noteId) {
-                console.log(allNotes[i]);
+        allNotes.forEach(note => {
+            if (note.id == noteId) {
+                console.log(allNotes);
             }
-        }
+        })
     })
 }); 
 
